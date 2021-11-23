713c20a4eede6fabc9114e6f5c5c03f6c9c2414f
Rohit <rohitkumarrsoni1231@gmail.com>
2021-11-23 14:31:40 +0500

    initial Commit

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by MacBook on 23/11/2021.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("Button was Pressed")
+    }
+    
+
+}
+
